815368c1a81ee35aa225130c4cbabb349d28f933
AsgerHL <asgeman@gmail.com>
2023-05-25 12:48:34 +0200

    sådan

--- Users.Test/UnitTest1.cs
+++ Users.Test/UnitTest1.cs
@@ -1,18 +1,47 @@
 using Users.Models;
+using Users.Controllers;
+using Users.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Users.Test;
 
 public class Tests
 {
+    private ILogger<UserController> _logger = null!;
+    private IConfiguration _configuration = null!;
+
     [SetUp]
     public void Setup()
     {
-        User u = new();
+        _logger = new Mock<ILogger<UserController>>().Object;
+
+        var myConfiguration = new Dictionary<string, string?>
+        {
+            {"UserServiceBrokerHost", "http://testhost.local"}
+        };
+
+         _configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(myConfiguration)
+            .Build();
+
+        
     }
 
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+    public void TestUserEndPoint_ValidUser()
+    {  
+        
+       
+         
+
+        
     }
 }
