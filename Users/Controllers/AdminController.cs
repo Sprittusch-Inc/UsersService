461826282d536a73960939f8c6c4283124ba2e22
AsgerHL <asgeman@gmail.com>
2023-06-01 19:33:41 +0200

    ny controller og endepunkter

--- Users/Controllers/AdminController.cs
+++ Users/Controllers/AdminController.cs
@@ -13,14 +13,13 @@ namespace Users.Controllers;
 [Route("api/[controller]")]
 
 
-public class UserController : ControllerBase
+public class AdminController : ControllerBase
 {
 
     private readonly IConfiguration _config;
     private readonly ILogger<UserController> _logger;
     protected static IMongoClient _client;
     protected static IMongoDatabase _db;
-    private readonly UserService _uService;
     private readonly AdminService _aService;
     private static string? _connString;
     private Hashing hashing = new();
@@ -28,7 +27,7 @@ public class UserController : ControllerBase
 
 
 
-    public UserController(ILogger<UserController> logger, IConfiguration config)
+    public AdminController(ILogger<UserController> logger, IConfiguration config)
     {
         /*
         Vault vault = new Vault(config);
@@ -46,53 +45,28 @@ public class UserController : ControllerBase
         //Henter user databasen fra mongodb
         _db = _client.GetDatabase("user");
 
-        //Henter kollektion users
-        var _uCollection = _db.GetCollection<User>("users");
-
         //Henter kollektion 
         var _aCollection = _db.GetCollection<Admin>("admin");
-        _uService = new UserService(_logger, _uCollection);
+        
         _aService = new AdminService(_logger, _aCollection);
         
     }
 
-    
-
     [AllowAnonymous]
-    [HttpPost("user")]
-    public async Task CreateUser(User u)
+    [HttpPost("admin")]
+    public async Task CreateAdmin(Admin a)
     {
-        await _uService.CreateUser(u);
-    }
-
-    [Authorize(Roles = "User")]
-    [HttpGet("user")]
-    public async Task GetUser(User u)
-    {
-        await _uService.GetUser(u);
-    }
-
-    [Authorize(Roles = "User")]
-    [HttpPut("user")]
-    public async Task UpdateUser(User u)
-    {
-        await _uService.UpdateUser(u);
-    }
-
-    [Authorize(Roles = "User")]
-    [HttpPut("user")]
-    public async Task DeleteUser(User u)
-    {
-        await _uService.DeleteUser(u);
+        await _aService.CreateAdmin(a);
     }
 
     
-
+    [Authorize(Roles = "Admin")]
+    [HttpPut("Admin")]
     
-
-   
-
-
+    public async Task DeleteAdmin(Admin a)
+    {
+        await _aService.DeleteAdmin(a);
+    }
 
 }
 
